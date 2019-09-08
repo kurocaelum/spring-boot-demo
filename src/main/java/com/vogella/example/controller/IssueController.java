35f07323dc1385ded87eae7d974015db16e4315b
Victor Simplicio <kuro_caelum@outlook.com>
2019-09-08 18:23:13 -0300

    Step 7 done

--- src/main/java/com/vogella/example/controller/IssueController.java
+++ src/main/java/com/vogella/example/controller/IssueController.java
@@ -4,27 +4,37 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.vogella.example.entity.IssueReport;
+import com.vogella.example.repositories.IssueRepository;
 
-@Controller                     // 
+@Controller                      
 public class IssueController {
+	IssueRepository issueRepository;	/* Auto-injetado no construtor pelo Spring */
+	
+	public IssueController(IssueRepository issueRepository) {
+		this.issueRepository = issueRepository;
+	}
 
-    @GetMapping("/issuereport") // 
-    public String getReport(Model model) { // 
+	@GetMapping("/issuereport")
+    public String getReport(Model model, @RequestParam(name = "submitted", required = false) boolean submitted) {
+        model.addAttribute("submitted", submitted);
         model.addAttribute("issuereport", new IssueReport());
-    	return "issues/issuereport_form";
+        return "issues/issuereport_form";
     }
 
-    @PostMapping("/issuereport") // 
-    public String submitReport(IssueReport issueReport, Model model) { // 
-        model.addAttribute("issuereport", new IssueReport());
-        model.addAttribute("submitted", true);
-    	return "issues/issuereport_form";
+    @PostMapping(value="/issuereport")
+    public String submitReport(IssueReport issueReport, RedirectAttributes ra) {
+        this.issueRepository.save(issueReport);
+        ra.addAttribute("submitted", true);
+        return "redirect:/issuereport";
     }
 
     @GetMapping("/issues")
-    public String getIssues(Model model) {  // 
-        return "issues/issuereport_list";
+    public String getIssueReport(Model model) {
+        model.addAttribute("issues", this.issueRepository.findAllButPrivate());
+       return "issues/issuereport_list";
     }
 }
